00450e302501e59ddf7427b9230c9cfeb226ee5d
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2022-12-10 12:27:30 +0000

    reshaping arays from 3d>2d with hilbert curves

--- src/array_reshape.rs
+++ src/array_reshape.rs
@@ -8,25 +8,22 @@ const ORDER: u8 = 64;
 pub fn image_to_line<'a>(image: &'a ArrayView3<'a, u8>) -> Array2<u8> {
     let im_shape = image.shape();
     let long_side = im_shape[0].max(im_shape[1]).next_power_of_two();
-    let mut flat_img: Array2<u8> = Array2::zeros(
-        (long_side.pow(2), im_shape[2])
-    );
+    let mut flat_img: Array2<u8> = Array2::zeros((long_side.pow(2), im_shape[2]));
 
     image.indexed_iter().for_each(|((x, y, z), &c)| {
         let t = fast_hilbert::xy2h(x as u32, y as u32, ORDER) as usize;
         flat_img[[t, z]] = c
-    }
-    );
-
-
+    });
 
     flat_img
 }
 
-pub fn line_to_image<'a>(image: &'a ArrayView2<'a, u8>, imshape1: usize, imshape2: usize) -> Array3<u8> {
-    let mut out_img: Array3<u8> = Array3::zeros((
-        imshape1, imshape2, image.shape()[1]
-    ));
+pub fn line_to_image<'a>(
+    image: &'a ArrayView2<'a, u8>,
+    imshape1: usize,
+    imshape2: usize,
+) -> Array3<u8> {
+    let mut out_img: Array3<u8> = Array3::zeros((imshape1, imshape2, image.shape()[1]));
 
     out_img.indexed_iter_mut().for_each(|((x, y, z), c)| {
         let t = fast_hilbert::xy2h(x as u32, y as u32, ORDER) as usize;
@@ -37,8 +34,8 @@ pub fn line_to_image<'a>(image: &'a ArrayView2<'a, u8>, imshape1: usize, imshape
 
 #[cfg(test)]
 mod tests {
-    use ndarray::Array3;
     use crate::array_reshape;
+    use ndarray::Array3;
 
     static US_U8_MAX: usize = u8::MAX as usize;
 
@@ -52,10 +49,7 @@ mod tests {
     #[test]
     fn image_to_line_to_image_square() {
         let (sh1, sh2, sh3) = (50usize, 50usize, 3usize);
-        let arr: Array3<u8> = Array3::from_shape_fn(
-            (sh1, sh2, sh3),
-            |(a, b, c)| odd_func(a, b, c),
-        );
+        let arr: Array3<u8> = Array3::from_shape_fn((sh1, sh2, sh3), |(a, b, c)| odd_func(a, b, c));
         let line = array_reshape::image_to_line(&arr.view());
         let arr2 = array_reshape::line_to_image(&line.view(), sh1, sh2);
         assert!(arr.eq(&arr2))
@@ -64,10 +58,7 @@ mod tests {
     #[test]
     fn image_to_line_to_image_rectangle() {
         let (sh1, sh2, sh3) = (50usize, 80usize, 3usize);
-        let arr: Array3<u8> = Array3::from_shape_fn(
-            (sh1, sh2, sh3),
-            |(a, b, c)| odd_func(a, b, c),
-        );
+        let arr: Array3<u8> = Array3::from_shape_fn((sh1, sh2, sh3), |(a, b, c)| odd_func(a, b, c));
         let line = array_reshape::image_to_line(&arr.view());
         let arr2 = array_reshape::line_to_image(&line.view(), sh1, sh2);
         assert!(arr.eq(&arr2))
