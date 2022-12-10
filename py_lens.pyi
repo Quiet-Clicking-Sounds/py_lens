c56b3dc612f40879f0f74604e2cbe52841fce6b7
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2022-12-10 12:25:00 +0000

    added image modifications RMS & stdev & 3d>2d

--- py_lens.pyi
+++ py_lens.pyi
@@ -1,45 +1,47 @@
 import numpy
 
 
+def wave_point(py_img: numpy.ndarray, ctr_x: float, ctr_y: float, u: float) -> numpy.ndarray:
+    """
+
+    :param py_img: image as array, expects shape() = (x,y,3)
+    :param ctr_x: pixel centre position
+    :param ctr_y: pixel centre position
+    :param u:
+    """
+    ...
+
+# not implemented
+# def wave_line(py_img: numpy.ndarray, ctr_x: float, ctr_y: float, angle: float, u: float) -> numpy.ndarray:
+#     """
+#     :param py_img: image as array, expects shape() = (x,y,3)
+#     :param ctr_x: pixel centre position
+#     :param ctr_y: pixel centre position
+#     :param angle: angle in radians
+#     :param u:
+#     """
+#     ...
+
+def star_pattern(py_img: numpy.ndarray, ctr_x: float, ctr_y: float, point_count: int, u: float) -> numpy.ndarray:
+    """
+    :param py_img: image as array, expects shape() = (x,y,3)
+    :param ctr_x: pixel centre position
+    :param ctr_y: pixel centre position
+    :param point_count: number of points in the pattern
+    :param u:
+    """
+    ...
 
 
-class WaveLine:
-    def __init__(self, ctr_x: float, ctr_y: float, angle: float, u: float):
-        """
-
-        :param ctr_x: pixel centre position
-        :param ctr_y: pixel centre position
-        :param angle: angle in radians
-        :param u:
-        """
-        ...
-    def apply_wave(self, py_img: numpy.ndarray):
-        """
-        :param py_img: image as array, expects shape() = (x,y,3)
-        """
-        ...
-class WavePoint:
-    def __init__(self, ctr_x:float, ctr_y:float, u:float):
-        """
-
-        :param ctr_x: pixel centre position
-        :param ctr_y: pixel centre position
-        :param u:
-        """
-        ...
-    def apply_wave(self, py_img: numpy.ndarray):
-        """
-        :param py_img: image as array, expects shape() = (x,y,3)
-        """
-        ...
-
-def image_to_line(py_img:numpy.ndarray)-> numpy.ndarray:
+def image_to_line(py_img: numpy.ndarray) -> numpy.ndarray:
     """
     :param py_img: 3d numpy array
     :return: 2d numpy array,
     """
     ...
-def line_to_image(py_img:numpy.ndarray, shape0:int, shape1:int)-> numpy.ndarray:
+
+
+def line_to_image(py_img: numpy.ndarray, shape0: int, shape1: int) -> numpy.ndarray:
     """
     :param shape1:  shape of output image (should be the same as what was put into image_to_line)
     :param shape0:  shape of output image (should be the same as what was put into image_to_line)
@@ -49,27 +51,63 @@ def line_to_image(py_img:numpy.ndarray, shape0:int, shape1:int)-> numpy.ndarray:
     ...
 
 
+def windowed_rms_single(py_img: numpy.ndarray, window_size: int, ddof1: bool) -> numpy.ndarray:
+    """
+    rms function over a windowed array, based on image processing, ignores the 3rd layer
+
+    :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
+    :param window_size: window size, this will create a moving window of shape [w,w,1] to traverse the array
+    :param ddof1: if true set ddof value to 1, else ddof is 0
+    :return: numpy array with dimensions of the input[x,y,z] - [w-1, w-1, 0]
+    """
+
+
+def windowed_rms_double(py_img: numpy.ndarray, window_size: tuple[int, int], ddof1: bool) -> numpy.ndarray:
+    """
+    rms function over a windowed array, based on image processing, ignores the 3rd layer
+
+    :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
+    :param window_size: window size, this will create a moving window of shape [w0,w1,1] to traverse the array
+    :param ddof1: if true set ddof value to 1, else ddof is 0
+    :return: numpy array with dimensions of the input[x,y,z] - [w0-1, w1-1, 0]
+    """
+
+
+def windowed_rms_triple(py_img: numpy.ndarray, window_size: tuple[int, int, int], ddof1: bool) -> numpy.ndarray:
+    """
+    rms function over a windowed array,
+
+    :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
+    :param window_size: window size, this will create a moving window of shape [w0,w1,w2] to traverse the array
+    :param ddof1: if true set ddof value to 1, else ddof is 0
+    :return: numpy array with dimensions of the input[x,y,z] - [w0-1, w1-1, w2-1]
+    """
+
 def windowed_stdev_single(py_img: numpy.ndarray, window_size: int) -> numpy.ndarray:
     """
     standard deviation over a windowed array, based on image processing, ignores the 3rd layer
 
     :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
-    :param window_size: window size, this will create a moiving window of shape [w,w,1] to traverse the array
+    :param window_size: window size, this will create a moving window of shape [w,w,1] to traverse the array
     :return: numpy array with dimensions of the input[x,y,z] - [w-1, w-1, 0]
     """
-def windowed_stdev_double(py_img:numpy.ndarray, window_size:tuple[int,int])-> numpy.ndarray:
+
+
+def windowed_stdev_double(py_img: numpy.ndarray, window_size: tuple[int, int]) -> numpy.ndarray:
     """
     standard deviation over a windowed array, based on image processing, ignores the 3rd layer
 
     :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
-    :param window_size: window size, this will create a moiving window of shape [w0,w1,1] to traverse the array
+    :param window_size: window size, this will create a moving window of shape [w0,w1,1] to traverse the array
     :return: numpy array with dimensions of the input[x,y,z] - [w0-1, w1-1, 0]
     """
-def windowed_stdev_triple(py_img:numpy.ndarray, window_size:tuple[int,int,int])-> numpy.ndarray:
+
+
+def windowed_stdev_triple(py_img: numpy.ndarray, window_size: tuple[int, int, int]) -> numpy.ndarray:
     """
     standard deviation over a windowed array,
 
     :param py_img: input image, must be a numpy.ndarray with 3 dimensions `len(array.shape)==3`
-    :param window_size: window size, this will create a moiving window of shape [w0,w1,w2] to traverse the array
+    :param window_size: window size, this will create a moving window of shape [w0,w1,w2] to traverse the array
     :return: numpy array with dimensions of the input[x,y,z] - [w0-1, w1-1, w2-1]
     """
--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,67 @@
 use numpy::{PyArray2, PyArray3, PyReadonlyArray2, PyReadonlyArray3, ToPyArray};
-use pyo3::prelude::{pyclass, pymethods, pymodule, pyfunction};
+use pyo3::prelude::{pyfunction, pymodule};
 use pyo3::prelude::{PyModule, PyResult, Python};
-use pyo3::{wrap_pyfunction};
+use pyo3::wrap_pyfunction;
 
-mod lens;
 mod array_reshape;
+mod lens;
 mod window;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn py_lens(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<WavePoint>()?;
-    m.add_class::<WaveLine>()?;
+    m.add_function(wrap_pyfunction!(wave_point, m)?)?;
+    //m.add_function(wrap_pyfunction!(wave_line, m)?)?;
+    m.add_function(wrap_pyfunction!(star_pattern, m)?)?;
     m.add_function(wrap_pyfunction!(image_to_line, m)?)?;
     m.add_function(wrap_pyfunction!(line_to_image, m)?)?;
+    m.add_function(wrap_pyfunction!(windowed_rms_single, m)?)?;
+    m.add_function(wrap_pyfunction!(windowed_rms_double, m)?)?;
+    m.add_function(wrap_pyfunction!(windowed_rms_triple, m)?)?;
     m.add_function(wrap_pyfunction!(windowed_stdev_single, m)?)?;
     m.add_function(wrap_pyfunction!(windowed_stdev_double, m)?)?;
     m.add_function(wrap_pyfunction!(windowed_stdev_triple, m)?)?;
-    #[pyclass]
-    struct WavePoint {
-        _tar: lens::WavePoint,
-    }
-    #[pymethods]
-    impl WavePoint {
-        #[new]
-        fn new(ctr_x: f64, ctr_y: f64, u: f64) -> Self {
-            WavePoint {
-                _tar: lens::WavePoint::new((ctr_x, ctr_y), u),
-            }
-        }
-        fn apply_wave<'py>(
-            &self,
-            py_img: PyReadonlyArray3<u8>,
-            py: Python<'py>,
-        ) -> &'py PyArray3<u8> {
-            let image_array = py_img.as_array();
-            let image_out = lens::lens_rgb(&image_array, self._tar);
-            image_out.to_pyarray(py)
-        }
-    }
 
-    #[pyclass]
-    struct WaveLine {
-        _tar: lens::WaveLine,
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, ctr_x, ctr_y, u, /)")]
+    fn wave_point<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        ctr_x: f64,
+        ctr_y: f64,
+        u: f64,
+    ) -> &'py PyArray3<u8> {
+        let wp = lens::WavePoint::new((ctr_x, ctr_y), u);
+        let img_out = lens::lens_rgb(&py_img.as_array(), wp);
+        img_out.to_pyarray(py)
     }
-    #[pymethods]
-    impl WaveLine {
-        #[new]
-        fn new(ctr_x: f64, ctr_y: f64, angle: f64, u: f64) -> Self {
-            WaveLine {
-                _tar: lens::WaveLine::new((ctr_x, ctr_y), angle, u),
-            }
-        }
-        fn apply_wave<'py>(&self, py: Python<'py>, py_img: PyReadonlyArray3<u8>)
-                           -> &'py PyArray3<u8> {
-            let image_array = py_img.as_array();
-            let image_out = lens::lens_rgb(&image_array, self._tar);
-            image_out.to_pyarray(py)
-        }
+    //#[pyfunction]
+    // this is unimplemented
+    fn wave_line<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        ctr_x: f64,
+        ctr_y: f64,
+        angle: f64,
+        u: f64,
+    ) -> &'py PyArray3<u8> {
+        let wp = lens::WaveLine::new((ctr_x, ctr_y), angle, u);
+        let img_out = lens::lens_rgb(&py_img.as_array(), wp);
+        img_out.to_pyarray(py)
+    }
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, ctr_x, ctr_y, point_count, u, /)")]
+    fn star_pattern<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        ctr_x: f64,
+        ctr_y: f64,
+        point_count: usize,
+        u: f64,
+    ) -> &'py PyArray3<u8> {
+        let wp = lens::StarPattern::new((ctr_x, ctr_y), point_count, u);
+        let img_out = lens::lens_rgb(&py_img.as_array(), wp);
+        img_out.to_pyarray(py)
     }
 
     #[pyfunction]
@@ -67,36 +71,110 @@ fn py_lens(_py: Python, m: &PyModule) -> PyResult<()> {
     }
 
     #[pyfunction]
-    fn line_to_image<'py>(py: Python<'py>, py_img: PyReadonlyArray2<u8>, shape0: usize, shape1: usize)
-                          -> &'py PyArray3<u8> {
-        let image_out = array_reshape::line_to_image(
-            &py_img.as_array(), shape0, shape1,
-        );
+    #[pyo3(text_signature = "(py_img, shape0, shape1 /)")]
+    fn line_to_image<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray2<u8>,
+        shape0: usize,
+        shape1: usize,
+    ) -> &'py PyArray3<u8> {
+        let image_out = array_reshape::line_to_image(&py_img.as_array(), shape0, shape1);
 
         image_out.to_pyarray(py)
     }
 
     #[pyfunction]
-    fn windowed_stdev_single<'py>(py: Python<'py>, py_img: PyReadonlyArray3<u8>, window_size: usize) -> &'py PyArray3<u8> {
+    #[pyo3(text_signature = "(py_img, window_size, /)")]
+    fn windowed_rms_single<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: usize,
+    ) -> &'py PyArray3<u8> {
         let window_type = window::WindowShape::Single(window_size);
-        windowed_stdev(py, py_img, window_type)
-    }
-    #[pyfunction]
-    fn windowed_stdev_double<'py>(py: Python<'py>, py_img: PyReadonlyArray3<u8>, window_size: (usize, usize)) -> &'py PyArray3<u8> {
-        let window_type = window::WindowShape::Double(window_size.0, window_size.1);
-        windowed_stdev(py, py_img, window_type)
-    }
-    #[pyfunction]
-    fn windowed_stdev_triple<'py>(py: Python<'py>, py_img: PyReadonlyArray3<u8>, window_size: (usize, usize, usize)) -> &'py PyArray3<u8> {
-        let window_type = window::WindowShape::Triple(window_size.0, window_size.1, window_size.2);
-        windowed_stdev(py, py_img, window_type)
+        windowed_rms(py, py_img, window_type)
     }
 
-    fn windowed_stdev<'py>(py: Python<'py>, py_img: PyReadonlyArray3<u8>, window_type: window::WindowShape) -> &'py PyArray3<u8> {
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, window_size, /)")]
+    fn windowed_rms_double<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: (usize, usize),
+    ) -> &'py PyArray3<u8> {
+        let window_type = window::WindowShape::Double(window_size.0, window_size.1);
+        windowed_rms(py, py_img, window_type)
+    }
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, window_size, /)")]
+    fn windowed_rms_triple<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: (usize, usize, usize),
+    ) -> &'py PyArray3<u8> {
+        let window_type = window::WindowShape::Triple(window_size.0, window_size.1, window_size.2);
+        windowed_rms(py, py_img, window_type)
+    }
+
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, window_size, ddof1, /)")]
+    fn windowed_stdev_single<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: usize,
+        ddof1: bool,
+    ) -> &'py PyArray3<u8> {
+        let window_type = window::WindowShape::Single(window_size);
+        windowed_stdev(py, py_img, window_type, ddof1)
+    }
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, window_size, ddof1, /)")]
+    fn windowed_stdev_double<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: (usize, usize),
+        ddof1: bool,
+    ) -> &'py PyArray3<u8> {
+        let window_type = window::WindowShape::Double(window_size.0, window_size.1);
+        windowed_stdev(py, py_img, window_type, ddof1)
+    }
+    #[pyfunction]
+    #[pyo3(text_signature = "(py_img, window_size, ddof1, /)")]
+    fn windowed_stdev_triple<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_size: (usize, usize, usize),
+        ddof1: bool,
+    ) -> &'py PyArray3<u8> {
+        let window_type = window::WindowShape::Triple(window_size.0, window_size.1, window_size.2);
+        windowed_stdev(py, py_img, window_type, ddof1)
+    }
+
+    fn windowed_rms<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_type: window::WindowShape,
+    ) -> &'py PyArray3<u8> {
         let image_out = window::thread_apply_over_window(
             py_img.to_owned_array(),
             window_type,
-            window::window_apply_methods::faster_stdev,
+            window::window_methods::faster_rms_u64_adding,
+        );
+        image_out.to_pyarray(py)
+    }
+
+    fn windowed_stdev<'py>(
+        py: Python<'py>,
+        py_img: PyReadonlyArray3<u8>,
+        window_type: window::WindowShape,
+        ddof1: bool,
+    ) -> &'py PyArray3<u8> {
+        let image_out = window::thread_apply_over_window(
+            py_img.to_owned_array(),
+            window_type,
+            match ddof1 {
+                false => window::window_methods::stdev_ddof_0,
+                true => window::window_methods::stdev_ddof_1,
+            },
         );
         image_out.to_pyarray(py)
     }
